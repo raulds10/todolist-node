a802971cc4f9d40e9378b9ab61105b974dcc7adb
Pablo Caro <pablo.caro@onetree.com>
2022-08-26 11:53:15 -0300

    local config

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,13 @@
 const User = require("../models/user");
 const sharp = require("sharp");
-const { sendWelcomeEmail, sendCancelationEmail } = require("../emails/account");
+// const { sendWelcomeEmail, sendCancelationEmail } = require("../emails/account");
 
 exports.register = async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
-    sendWelcomeEmail(user.email, user.name);
+    // sendWelcomeEmail(user.email, user.name);
     const token = await user.generateAuthToken();
     res.status(201).json({ user, token });
   } catch (err) {
@@ -110,7 +110,7 @@ exports.getImage = async (req, res) => {
 exports.deleteUser = async (req, res) => {
   try {
     await req.user.remove();
-    sendCancelationEmail(req.user.email, req.user.name);
+    // sendCancelationEmail(req.user.email, req.user.name);
     res.send(req.user);
   } catch (e) {
     res.status(500).send();
