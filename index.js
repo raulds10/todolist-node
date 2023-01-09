0a82dd857acc26e333d1be94080dc73211f35a58
raul de serranos <rdeserranos@iuvity.net>
2023-01-08 22:20:27 -0300

    change index.js

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const cors = require("cors");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
+//const dotenv = require("dotenv");
+//const mongoose = require('mongoose');
+
+//dotenv.config({path: "./config.env"});
+//const PORT = process.env.PORT || 3000
+//const DB = process.env.MONGODB_URL;  
+
+//mongoose
+//  .connect(DB, {
+//    usenewurlparser: true,
+//    useCreateIndex: true,
+//    useunifiedtopology: true,
+//  })
+//  .then(() => {
+//    console.log("Successfully connected ");
+//  })
+//  .catch((error) => {
+//    console.log(`can not connect to database, ${error}`);
+//  });
 
 const app = express();
 const port = process.env.PORT;
@@ -16,6 +35,6 @@ app.use(express.json());
 app.use("/user", userRouter);
 app.use("/task", taskRouter);
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is up on port " + port);
 });
